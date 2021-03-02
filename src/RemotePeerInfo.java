983d4005991a2f19e31b717af4a35515cf4d2bed
ypchen520 <ypchen520@gmail.com>
2021-03-02 12:27:44 -0500

    Update RemotePeerInfo.java

--- src/RemotePeerInfo.java
+++ src/RemotePeerInfo.java
@@ -8,13 +8,13 @@
  */
 
 public class RemotePeerInfo {
-	public String peerId;
-	public String peerAddress;
-	public String peerPort;
-	
-	public RemotePeerInfo(String pId, String pAddress, String pPort) {
-		peerId = pId;
-		peerAddress = pAddress;
-		peerPort = pPort;
-	}
+    public String peerId;
+    public String peerAddress;
+    public String peerPort;
+    
+    public RemotePeerInfo(String pId, String pAddress, String pPort) {
+        peerId = pId;
+        peerAddress = pAddress;
+        peerPort = pPort;
+    }
 }
