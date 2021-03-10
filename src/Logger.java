f1e174506877c6f7fa331fd987813ed0aac52480
yupengchen <ypchen520@gmail.com>
2021-03-09 20:27:20 -0500

    Finished implementing the Logger

--- src/Logger.java
+++ src/Logger.java
@@ -1,6 +1,9 @@
 import java.util.Date;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -8,18 +11,18 @@ import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 
 public class Logger {
-    private int peerId;
+    private int peerID;
     private List<String> log = new ArrayList<>();
     private String filePrefix = "log_peer_";
     private String fileExtenstion = ".log";
 
-    public Logger(int peerId){
-        this.peerId = peerId;
+    public Logger(int peerID){
+        this.peerID = peerID;
     }
 
     private String formatDate(Date date){
         SimpleDateFormat dateFormat = new SimpleDateFormat ("yyyy-MM-dd hh:mm:ss a zzz");
-        return dateFormat.format(date);
+        return dateFormat.format(date) + ": ";
     }
 
     private void writeToLog(String content) throws IOException{
@@ -27,8 +30,8 @@ public class Logger {
     }
 
     private void writeToFile() throws IOException{
-        FileWriter fileWriter = new FileWriter(filePrefix + peerId + fileExtenstion);
-        // PrintWriter printWriter = new PrintWriter(fileWriter);
+        String filename = filePrefix + peerID + fileExtenstion;
+        FileWriter fileWriter = new FileWriter(filename, true);
         BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
         PrintWriter printWriter = new PrintWriter(bufferedWriter);
         for(String s : log){
@@ -38,17 +41,62 @@ public class Logger {
         printWriter.close();
     }
 
-    public void logTcpConnection(int peerId2, String tcpType) throws IOException{
-        
-        switch (tcpType) {
-        case "to":
-            writeToLog(formatDate(new Date()) + "Peer " + peerId + " makes a connection to Peer " + peerId2 + ".");
-            break;
-        case "from":
-            writeToLog(formatDate(new Date()) + "Peer " + peerId + " is connected from Peer " + peerId2 + ".");
-            break;
+    public void logTcpConnection(int peerID2, String messageType) throws IOException{
+        switch (messageType) {
+            case "to":
+                writeToLog(formatDate(new Date()) + "Peer " + peerID + " makes a connection to Peer " + peerID2 + ".");
+                break;
+            case "from":
+                writeToLog(formatDate(new Date()) + "Peer " + peerID + " is connected from Peer " + peerID2 + ".");
+                break;
+            default:
+                System.out.println("[logTcpConnection] Unknown message type");
         }
     }
     
-    
+    public void logPreferredNeighborsChange(List<RemotePeerInfo> preferredNeighbors) throws IOException{
+        String preferredNeighborIDs = "";
+        int n = preferredNeighbors.size();
+        for(int i = 0; i < n; i++){
+            preferredNeighborIDs += String.valueOf(preferredNeighbors.get(i).getPeerID());
+            if(i != n-1)
+                preferredNeighborIDs += ", ";
+        }
+        writeToLog(formatDate(new Date()) + "Peer " + peerID + " has the preferred neighbors " + preferredNeighborIDs + ".");
+    }
+
+    public void logTitForTat(int peerID2, String messageType) throws IOException{
+        switch (messageType) {
+            case "optUnchoke":
+                writeToLog(formatDate(new Date()) + "Peer " + peerID + " has the optimistically unchoked neighbor " + peerID2 + ".");
+                break;
+            case "unchoke":
+                writeToLog(formatDate(new Date()) + "Peer " + peerID + " is unchoked by " + peerID2 + ".");
+                break;
+            case "choke":
+                writeToLog(formatDate(new Date()) + "Peer " + peerID + " is choked by " + peerID2 + ".");
+                break;
+            default:
+                System.out.println("[logTitForTat] Unknown message type");
+        }
+    }
+
+    public void logReceivingMessages(int peerID2, String messageType) throws IOException{
+        final String[] setValues = new String[] { "have", "interested", "not interested" };
+        final Set<String> msgTypeSet = new HashSet<>(Arrays.asList(setValues));
+        if(!msgTypeSet.contains(messageType)){
+            System.out.println("[logTitForTat] Unknown message type");
+        }else{
+            writeToLog(formatDate(new Date()) + "Peer " + peerID + " received the '" + messageType + "' message from " + peerID2 + ".");
+        }
+    }
+
+    public void logDownloadingPiece(int peerID2, int pieceIndex, int numPieces) throws IOException{
+        writeToLog(formatDate(new Date()) + "Peer " + peerID + " has downloaded the piece " + pieceIndex + " from " + peerID2 + "." +
+                   " Now the number of pieces it has is " + numPieces + ".");
+    }
+
+    public void logCompleteDownloading() throws IOException{
+        writeToLog(formatDate(new Date()) + "Peer " + peerID + " has downloaded the complete file.");
+    }
 }
